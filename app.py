9051ac39c70b246f9f0863cd4f19db76ae4bd8f9
DavidMandado <d.mandado@student.tue.nl>
2025-12-09 07:36:17 +0100

    ....

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ df = pd.read_csv("data/CIA_DATA.csv")
 
 print("Checkpoint 1 ⇣")
 
+fig = px.bar(df, x="Country", y="Population_Below_Poverty_Line_percent", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+             Dash: A web application tatata
+             '''),
+    
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
